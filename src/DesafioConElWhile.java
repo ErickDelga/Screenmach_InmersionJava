6d4d41db04a674096b50b4edcad012e3f61957af
ErickDelga <erick007503@gmail.com>
2024-04-30 11:03:40 -0600

    Desafio de aula01 implementacion del While

--- src/DesafioConElWhile.java
+++ src/DesafioConElWhile.java
@@ -1,20 +1,9 @@
 import java.util.Scanner;
 
-public class Main {
+public class DesafioConElWhile {
     public static void main(String[] args) {
-
-        System.out.println("Bienvenido a la Inmersion Java");
         double mediaEvaluacionUsuario = 0;
         int i = 0;
-        /*for (i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Ingresa la nota que le darias a Matrix");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionUsuario += notaMatrix;
-        }
-        System.out.println("Tota global de " + i + " Usuarios= " + mediaEvaluacionUsuario);
-        System.out.println("La media de la pelicula matrix calculada por el ususario es= " + mediaEvaluacionUsuario/3);*/
-
         while (i < 5){
             Scanner teclado = new Scanner(System.in);
             System.out.println("Ingresa la nota que le darias a Matrix");
